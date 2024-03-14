60297cfcbe3edf3e8fa7bcb487e415e85df60654
bpuchala <bpuchala@users.noreply.github.com>
2024-03-14 00:35:57 -0400

    version 2.0a3

--- python/doc/conf.py
+++ python/doc/conf.py
@@ -3,7 +3,7 @@ import os
 # -- package specific configuration --
 project = "libcasm-mapping"
 version = "2.0"  # The short X.Y version.
-release = "2.0a2"  # The full version, including alpha/beta/rc tags.
+release = "2.0a3"  # The full version, including alpha/beta/rc tags.
 project_desc = "CASM structure mapping"
 logo_text = "libcasm-mapping"
 github_url = "https://github.com/prisms-center/CASMcode_mapping/"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="libcasm-mapping",
-    version="2.0a2",
+    version="2.0a3",
     packages=[
         "libcasm",
         "libcasm.mapping",
